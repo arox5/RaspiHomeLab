85fdfd56942435820cce0b17c5e62d58e2cafa40
arox5 <andrea.rossetti5@gmail.com>
2018-01-15 22:26:32 +0100

    1st php files commit

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+require 'config_local.php';
+require 'config_env.php';
+
+date_default_timezone_set('Europe/Rome');
+
+if(ENVIRONMENT=='TST'){
+	//windows dev environment
+	define("PICFOLDER", "../cam_files/");
+} else {
+	//raspberry
+	define("PICFOLDER", "/home/pi/FTP/cam/");
+}
+
+if(isset($local_cfg_is_set)){
+	//settings taken from file config_local.php (this file is not available in github)
+	$userinfo = $local_userinfo;
+
+	$camurl = $local_camurl;
+	$camport = $local_camport;
+	$camusr = $local_camusr;
+	$campwd = $local_campwd;
+
+	define("CAM_LAST_PIC_URL", "http://" . $camurl . ":" . $camport . "/image/jpeg.cgi");
+	define("CAM_URL", "http://" . $camusr . ":" . $campwd . "@" . $camurl . ":" . $camport . "/");	
+} else {
+	//if not set this 'no' will be used by index.php
+	$local_cfg_is_set = 'no';
+
+	//default user/password
+	$userinfo = array(
+		'usr1' => 'pwd1'
+		);	
+}
+?>
---
+++ config_env.php
@@ -0,0 +1,5 @@
+<?php
+//this file is different from dev and pro environment
+define("ENVIRONMENT", "TST");		//windows dev environment
+//define("ENVIRONMENT", "PRO");		//raspberry pro environment
+?>
