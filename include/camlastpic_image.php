d818dd2fd1a367516a0c9f97aba0f880f6fc4b2f
arox5 <andrea.rossetti5@gmail.com>
2018-01-20 22:19:03 +0100

    add time stamp to image

--- include/camlastpic_image.php
+++ include/camlastpic_image.php
@@ -1,10 +1,33 @@
 <?php require 'session.php' ?>
 <?php
 if(isset($isloggedin) && $isloggedin) {
-    $imageData = base64_encode(file_get_contents(CAM_LAST_PIC_URL));
-    header("Content-type: image/jpeg");
-    echo base64_decode($imageData);
-    //echo 'data:image/jpeg;base64,' . $imageData;
+    /*
+    $imageDataEncoded = base64_encode(file_get_contents(CAM_LAST_PIC_URL));
+    $imageData = base64_decode($imageDataEncoded);
+    $source = imagecreatefromstring($imageData);
+
+    //echo base64_decode($imageDataEncoded);
+    //echo 'data:image/jpeg;base64,' . $imageDataEncoded;
+    */
+
+    //create image from URL
+    $source = imagecreatefromstring(file_get_contents(CAM_LAST_PIC_URL));
+    //get image height (480)
+    $imgSY = ImageSY($source);
+    //set string color (black)
+    $textcolor = imagecolorallocate($source, 0, 0, 0);
+    //set background color (white)
+    $bg_color = imagecolorallocate($source, 211, 211, 211);
+    //draw background
+    imagefilledrectangle($source, 2, 2, 175, 19, $bg_color);
+    // Write the string at the bottom left 
+    imagestring($source, 5, 3, 3, date("d/m/Y H:i:s"), $textcolor);
+    //output
+    header("Content-type: image/png");
+    imagepng($source);
+    //release memory
+    imagedestroy($imageData);
+
 } else {
     //not authenticated, redirect to home page
     header('Location: ../');
