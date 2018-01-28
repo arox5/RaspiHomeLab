d36d8beee87a962cfb7c6b73f0e92d938db1a623
arox5 <andrea.rossetti5@gmail.com>
2018-01-28 21:45:39 +0100

    alignment

--- include/campiclist.php
+++ include/campiclist.php
@@ -1,35 +1,44 @@
 <?php require 'session.php' ?>
 <?php if(isset($isloggedin) && $isloggedin): ?>
-
     <!-- gallery navigation -->
-    <form>
-        <div id="nav-form" class="form-group">
-                <label>Images per page</label>
-                <select  id="picsperpage">
-                    <option selected>12</option>
-                    <option>24</option>
-                    <option>36</option>
-                    <option>48</option>
-                    <option>60</option>
-                </select>
-                &nbsp;&nbsp;
-                <label>Page <span id="curpage">1</span>/<span id="totalpages">?</span></label>
-                &nbsp;&nbsp;
+
+    <div id="nav-form">
+        <div id="row">
+            <div id="col" class="form-group">
                 <div class="btn-group btn-group-toggle">
                     <button type="button" id="goToFirst" class="btn btn-outline-dark">&nbsp;&laquo;&nbsp;</button>
                     <button type="button" id="goToPrev" class="btn btn-outline-dark">&nbsp;&lsaquo;&nbsp;</button>
                     <button type="button" id="goToNext" class="btn btn-outline-dark">&nbsp;&rsaquo;&nbsp;</button>
                     <button type="button" id="goToLast" class="btn btn-outline-dark">&nbsp;&raquo;&nbsp;</button>
                 </div>
+            </div> <!-- col -->
+            <div id="col" class="form-group">
+                <!--
+                <form class="form-inline">
+                -->
+                <form>
+                    <label>Images per page: </label>&nbsp;&nbsp;
+                    <!--
+                    <select class="form-control" style="width:70px; text-align:center" id="picsperpage">
+                    -->
+                    <select id="picsperpage">
+                        <option selected>12</option>
+                        <option>24</option>
+                        <option>36</option>
+                        <option>48</option>
+                        <option>60</option>
+                    </select>
+                </form>
+            </div> <!-- col -->
+            <div id="col" class="form-group">
+                <label>Page: <span id="curpage">1</span>/<span id="totalpages">?</span></label>
                 &nbsp;&nbsp;
-                <label>Total images <span id="totalpictures">?</span></label>
+                <label>Total images: <span id="totalpictures">?</span></label>
                 &nbsp;&nbsp;
                 <label><span id="spinner"></span></label>
-        </div>
-    
-        <br /><br />
-    </form>
-
+            </div> <!-- col -->
+        </div> <!-- row -->
+    </div>
     <!-- gallery container -->
     <div class="row" id="piclistgallery">
     </div>
